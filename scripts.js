c0499b49dc5e604de499d48d30f577787155f837
Emmett Herrick <123272376+Rinnemme@users.noreply.github.com>
2023-07-23 20:49:51 -0400

    put together test formatting for cards, write basic arrays and object constructors for objects containing info for each item of food, bars, etc

---
+++ scripts.js
@@ -0,0 +1,44 @@
+let foodObjects = []
+let barObjects = []
+let sceneryObjects = []
+let funObject = []
+
+function foodObject(name, link, distance, image, description) {
+    this.name = name
+    this.link = link
+    this.distance = distance
+    this.image = image
+    this.description = description
+    foodObjects.push(this)
+}
+
+function barObject(name, link, distance, image, description) {
+    this.name = name
+    this.link = link
+    this.distance = distance
+    this.image = image
+    this.description = description
+    barObjects.push(this)
+}
+
+function sceneryObject(name, link, image, description, first, second, third) {
+    this.name = name
+    this.link = link
+    this.image = image
+    this.description = description
+    this.first = first
+    this.second = second
+    this.third = third
+    sceneryObjects.push(this)
+}
+
+function funObject(name, link, image, description, first, second, third) {
+    this.name = name
+    this.link = link
+    this.image = image
+    this.description = description
+    this.first = first
+    this.second = second
+    this.third = third
+    funObjects.push(this)
+}
